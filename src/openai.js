ba24dc17e4eb9479fec427a0b505976cb5f20d0e
Firdavs Abdulloev <firdavsabdulloev@Firdavss-MacBook-Pro.local>
2023-04-30 12:40:45 +0500

    initial GPT BOT

---
+++ src/openai.js
@@ -0,0 +1,44 @@
+import {Configuration, OpenAIApi} from "openai"
+import config from 'config'
+import {createReadStream} from 'fs'
+import {removeFile} from "./utils.js"
+
+class OpenAI {
+    roles = {
+        ASSISTANT: 'assistant',
+        USER: 'user',
+        SYSTEM: 'system'
+    }
+
+    constructor(apiKey) {
+        const configuration = new Configuration({apiKey})
+        this.openai = new OpenAIApi(configuration)
+    }
+
+    async chat(messages) {
+        try {
+            const response = await this.openai.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages,
+            })
+            return response.data.choices[0].message
+        } catch (e) {
+            console.log('Error while chat with gpt', e.message)
+        }
+    }
+
+    async transcription(filepath) {
+        try {
+            const response = await this.openai.createTranscription(
+                createReadStream(filepath),
+                'whisper-1'
+            )
+            removeFile(filepath)
+            return response.data.text
+        } catch (e) {
+            console.log('Error while transcription process', e.message)
+        }
+    }
+}
+
+export const openai = new OpenAI(config.get('OPENAI_API_KEY'))
